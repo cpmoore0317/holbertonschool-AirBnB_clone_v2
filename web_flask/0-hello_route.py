a59f329046a3fc50407078c1871523ffd2082312
cpmoore0317 <kingpkayy@gmail.com>
2023-11-13 14:30:56 -0600

    Shebang changes for tasks0-1, adding script task1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 from flask import Flask
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnbL():
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
