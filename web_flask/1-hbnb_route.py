ab02a4b36d76ef72a148e51feef8c66f1390dea2
cpmoore0317 <kingpkayy@gmail.com>
2023-11-14 14:30:57 -0600

    PEP8 for 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" A script that starts a Flask web app to display a text depending on the route """
+""" A script that starts a Flask web app to display a text """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnbL():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
