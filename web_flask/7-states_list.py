b45c178c858157a48195eae3198ba182f88de6b8
cpmoore0317 <kingpkayy@gmail.com>
2023-11-14 13:34:45 -0600

    Line spacing changes for task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,6 @@ from models import storage
 from models.state import State
 from models import storage
 
-
 app = Flask(__name__)
 
 
@@ -14,9 +13,11 @@ def states_list():
     return render_template("7-states_list.html",
                            states=storage.all(State))
 
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
