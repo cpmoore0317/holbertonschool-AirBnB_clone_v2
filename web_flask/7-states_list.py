39ccafb18154439cf96550ac39dca14f50a9bd08
cpmoore0317 <kingpkayy@gmail.com>
2023-11-14 13:33:01 -0600

    Testing spacing change for documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application to display States"""
+""" Script that starts a Flask web application to display States """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
