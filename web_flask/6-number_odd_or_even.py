5fc39eb5f22f4aa891b3d3eb500b4b80a4ae37b7
cpmoore0317 <kingpkayy@gmail.com>
2023-11-14 14:33:36 -0600

    PEP8 for 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,19 +4,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     # Replace underscores with spaces
     display_text = text.replace('_', ' ')
     return 'C {}'.format(display_text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -25,17 +29,21 @@ def python_route(text):
     display_text = text.replace('_', ' ')
     return 'Python {}'.format(display_text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     return '{} is a number'.format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_route(n):
-        return render_template('5-template.html', number=n)
+    return render_template('5-template.html', number=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even_route(n):
-     return(render_template('6-number_odd_or_even.html', n=n))
+    return(render_template('6-number_odd_or_even.html', n=n))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
